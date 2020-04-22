731454fa0dc2313187f9b471da188e2239852497
Abdulshaheed Alqunber <asq@bu.edu>
2020-04-22 17:11:17 -0400

    update variables

--- script.py
+++ script.py
@@ -3,10 +3,10 @@ import os
 
 file_name = os.listdir()
 print(file_name)
-password = "GD@3i$@44T!"
-correct_password = "GDaEis@44t!"
+old_password = "GD@3i$@44T!"
+new_password = "GDaEis@44t!"
 
 for file in file_name:
-    pdf = pikepdf.open(file, password=password)
-    pdf.save("NEW"+file, encryption=pikepdf.Encryption(owner=correct_password, user=correct_password, R=6))
+    pdf = pikepdf.open(file, password=old_password)
+    pdf.save("NEW"+file, encryption=pikepdf.Encryption(owner=new_password, user=new_password, R=6))
     pdf.close
